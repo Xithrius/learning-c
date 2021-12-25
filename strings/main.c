e298210c2781d4393bee89a23f71267ae58f5ba9
Xithrius <xithrius@gmail.com>
2021-12-25 00:48:56 -0800

    String stuff

---
+++ strings/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char* str = "testing";
+
+    printf("%s\n", str);
+
+    printf("The first character: %c\n", str[0]);
+
+    printf("All the characters in order:\n");
+
+    for (int i = 0; i < strlen(str); i++) {
+        printf("%d: %c\n", i, str[i]);
+    }
+
+    return 0;
+}
